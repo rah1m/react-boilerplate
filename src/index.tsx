6b0691b0f4963bde500b7e5c346d936768cedefe
rahimfirudunlu <rahim@workstreams.ai>
2022-05-08 18:49:50 +0400

    prettier & husky setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-import { store } from '@store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { store } from "@store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import './index.css';
+import "./index.css";
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
