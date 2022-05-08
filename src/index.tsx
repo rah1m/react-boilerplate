0d23b08c364adbf63247a1e572311dce33293d98
rahimfirudunlu <rahim@workstreams.ai>
2022-05-08 18:39:19 +0400

    setup cra

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+
+import { store } from '@store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import './index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
